b1adf3e9a7acae167a5121d8c9766d0d79bb85cc
Saikat <saikatban91@gmail.com>
2017-08-27 14:08:00 -0500

    edited the cachematrix.R file

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,51 @@
-## Put comments here that give an overall description of what your
-## functions do
+## In the functions below, the  <<- operator is used 
+## to assign values to objects "i" and "x" in a different environment
+## from the current environment they are in. The two functions below 
+## can compute and cache the inverse of a matrix and provide a useful
+## solution to otherwise expensive and time-consuming computations. 
 
-## Write a short comment describing this function
+## The following makeCacheMatrix () function creates a special "matrix"
+## that can cache its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  i <- NULL
+  set <- function(y){
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) i <<- inverse
+  getinverse <- function() i
+ ## The following list is returned from this function and used as the 
+ ## input to cacheSolve()
+   list(set = set, get = get, setinverse = setinverse, 
+       getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
+## The following cacheSolve () function computes the inverse of
+## an invertible matrix that is the output of the
+## makeCacheMatrix()cach function. The first job it does is verify 
+## whether the inverse has been already computed.
+## If yes, then returns the inverse from the cache
+## and skips the computation. Or, it calculates the 
+## inverse of the matrix and sets the value of the 
+## inverse in the cache through the setinverse 
+## function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## input matrix to cacheSolve() is from the output of makeCacheMatrix()
+  i <- x$getinverse()
+  ## Checking whether the inverse has been 
+  ## calculated; if yes, then gets it from cache
+  ## otherwise continue.
+  if(!is.null(i)){
+    message("getting cached data")
+    return(i)
+  }
+  ## Computing the inverse after checking that
+  ## the value is not available
+  data <- x$get()
+  i <- solve(data, ...)
+  x$setinverse(i)
+  i
 }
